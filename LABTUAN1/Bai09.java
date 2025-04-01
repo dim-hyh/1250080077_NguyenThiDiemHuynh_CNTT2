b59f2d890d9b3bc972c62d3f13a8576b561daf87
dim-hyh <1250080077@sv.hcmunre.edu.vn>
2025-04-01 16:08:49 +0700

    Update Bai09.java

---
+++ LABTUAN1/Bai09.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Bai09 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Nhập số: ");
+        int so = sc.nextInt();
+
+        int tong = tinhTongChuSo(so);
+        int tich = tinhTichChuSo(so);
+
+        System.out.println("Tổng chữ số: " + tong);
+        System.out.println("Tích chữ số: " + tich);
+
+        sc.close();
+    }
+
+    static int tinhTongChuSo(int so) {
+        int tong = 0;
+        while (so > 0) {
+            tong += so % 10;
+            so /= 10;
+        }
+        return tong;
+    }
+
+    static int tinhTichChuSo(int so) {
+        int tich = 1;
+        while (so > 0) {
+            tich *= so % 10;
+            so /= 10;
+        }
+        return tich;
+    }
+}
