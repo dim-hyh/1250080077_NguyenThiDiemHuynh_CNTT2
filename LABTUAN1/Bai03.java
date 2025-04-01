9f66a06bcff6391c84873666538288dff049e513
dim-hyh <1250080077@sv.hcmunre.edu.vn>
2025-04-01 15:43:23 +0700

    Update and rename buoi1b3.java to Bai03.java

--- LABTUAN1/Bai03.java
+++ LABTUAN1/Bai03.java
@@ -1,5 +1,5 @@
 import java.utill.Scanner;
-public class buoi1b3 {
+public class Bai03 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Nhập nhiệt độ F: ");
