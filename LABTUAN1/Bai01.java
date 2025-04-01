b9bd740d37cf6c267eaab3622fcbd8674d01ca8f
dim-hyh <1250080077@sv.hcmunre.edu.vn>
2025-04-01 15:24:40 +0700

    Create Bai01.java

---
+++ LABTUAN1/Bai01.java
@@ -0,0 +1,10 @@
+public class buoi1b1 {
+    public static void main (String [] args) {
+        String ten = "Nguyễn Thị Diễm Huỳnh";
+        String ngaysinh = "20/02/2005";
+        String mssv = "1250080077";
+        System.out.println("Họ tên:" + ten);
+        System.out.println("Ngày sinh:" + ngaysinh);
+        System.out.println("MSSV:" + mssv);
+    }
+} 
