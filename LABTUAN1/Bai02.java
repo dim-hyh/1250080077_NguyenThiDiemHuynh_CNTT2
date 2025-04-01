79903c870ad388d442b0e480e65a94029facd636
dim-hyh <1250080077@sv.hcmunre.edu.vn>
2025-04-01 15:42:15 +0700

    Update and rename Buoi02.java to Bai02.java

--- LABTUAN1/Bai02.java
+++ LABTUAN1/Bai02.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Buoi02  {
+public class Bai02  {
     public static void main(String [] args) {
         Scanner scanner = new Scanner (System.in);
         System.out.print ("Nhập chiều rộng:");
