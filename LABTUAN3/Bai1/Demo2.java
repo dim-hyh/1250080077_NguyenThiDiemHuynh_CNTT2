6b13eaaf021f3d149cf56378a9f2abd0a74b4e22
dim-hyh <1250080077@sv.hcmunre.edu.vn>
2025-04-21 11:03:21 +0700

    Update Demo2.java

--- LABTUAN3/Bai1/Demo2.java
+++ LABTUAN3/Bai1/Demo2.java
@@ -1,1 +1,14 @@
-
+public class Demo2 {
+    public static void main(String[] args) {
+        DanhSachHocSinh ds = new DanhSachHocSinh();
+        ds.nhapDanhSach();
+        ds.inDanhSach();
+        ds.doiTenHocSinh("Nguyen Van A", "Nguyen Van Z");
+        HocSinh hsMax = ds.timDiemCaoNhat();
+        System.out.println("\nHọc sinh có điểm trung bình cao nhất:");
+        hsMax.xuatHocSinh();
+        ds.sapXepGiamTheoDiem();
+        System.out.println("\nDanh sách sau khi sắp xếp giảm dần theo điểm:");
+        ds.inDanhSach();
+    }
+}
